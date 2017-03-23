7cea03fed609d4aa3f08a63a32720e2909397253
Peter Hellberg <peter@c7.se>
2017-03-23 20:18:24 +0100

    Initial code documentation

---
+++ titleservice/doc.go
@@ -0,0 +1,12 @@
+/*
+
+Package titleservice is a client for the MMS TitleService API
+
+Installation
+
+Just go get the package:
+
+    go get -u github.com/TV4/mms/titleservice
+
+*/
+package titleservice
