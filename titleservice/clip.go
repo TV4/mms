4fb2584f997fe03c50de473364f0825fb755e1d0
Peter Hellberg <peter@c7.se>
2017-03-23 17:54:19 +0100

    Clip struct fields

--- titleservice/clip.go
+++ titleservice/clip.go
@@ -2,6 +2,15 @@ package titleservice
 
 import "context"
 
-type Clip struct{}
+// Clip is a “stand-alone” short clip not linked to a series/season or other title
+type Clip struct {
+	TitleCode      string
+	Title          string
+	Length         int
+	PublishedAt    string
+	AvailableUntil string
+	Description    string
+	PlayURL        string
+}
 
 func (c *client) RegisterClip(ctx context.Context, clip Clip) {}
