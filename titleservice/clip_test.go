c2e5cbc57491a6c53864dd591901cffb373a0350
Peter Hellberg <peter@c7.se>
2017-03-24 17:59:59 +0100

    Test clip

--- titleservice/clip_test.go
+++ titleservice/clip_test.go
@@ -10,12 +10,18 @@ func TestRegisterClip(t *testing.T) {
 	var (
 		statusCode = http.StatusInternalServerError
 		hf         = testHandlerFunc(statusCode, nil)
+
+		clip = Clip{
+			TitleCode: "clip-title-code",
+			Title:     "clip-title",
+			Length:    1,
+		}
 	)
 
 	ts, c := testServerAndClient(testUser, testPass, hf)
 	defer ts.Close()
 
-	r, err := c.RegisterClip(context.Background(), Clip{})
+	r, err := c.RegisterClip(context.Background(), clip)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
