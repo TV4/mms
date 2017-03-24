62b1fc521593803fec4c83a794217e20133f94f2
Peter Hellberg <peter@c7.se>
2017-03-24 19:13:32 +0100

    Fix example code indentation

--- titleservice/doc.go
+++ titleservice/doc.go
@@ -16,56 +16,56 @@ Usage
 
 A small usage example:
 
-        package main
+	package main
 
-        import (
-        	"context"
-        	"encoding/json"
-        	"flag"
-        	"log"
-        	"os"
+	import (
+		"context"
+		"encoding/json"
+		"flag"
+		"log"
+		"os"
 
-        	titleservice "github.com/TV4/mms/titleservice"
-        )
+		titleservice "github.com/TV4/mms/titleservice"
+	)
 
-        func main() {
-        	var username, password string
+	func main() {
+		var username, password string
 
-        	flag.StringVar(&username, "user", "", "username")
-        	flag.StringVar(&password, "pass", "", "password")
+		flag.StringVar(&username, "user", "", "username")
+		flag.StringVar(&password, "pass", "", "password")
 
-        	flag.Parse()
+		flag.Parse()
 
-        	var (
-        		logger = log.New(os.Stderr, "", 0)
-        		enc    = json.NewEncoder(os.Stdout)
-        		ctx    = context.Background()
-        		c      = titleservice.NewClient(username, password, titleservice.Simulate)
+		var (
+			logger = log.New(os.Stderr, "", 0)
+			enc    = json.NewEncoder(os.Stdout)
+			ctx    = context.Background()
+			c      = titleservice.NewClient(username, password, titleservice.Simulate)
 
-        		clip = titleservice.Clip{
-        			TitleCode:   "123",
-        			Title:       "Test-title",
-        			Length:      1,
-        			PublishedAt: "20170324",
-        		}
-        	)
+			clip = titleservice.Clip{
+				TitleCode:   "123",
+				Title:       "Test-title",
+				Length:      1,
+				PublishedAt: "20170324",
+			}
+		)
 
-        	resp, err := c.RegisterClip(ctx, clip)
-        	if err != nil {
-        		logger.Fatal(resp, "\n", err)
-        	}
+		resp, err := c.RegisterClip(ctx, clip)
+		if err != nil {
+			logger.Fatal(resp, "\n", err)
+		}
 
-        	enc.SetIndent("", "  ")
-        	enc.Encode(resp)
-				}
+		enc.SetIndent("", "  ")
+		enc.Encode(resp)
+	}
 
 Output:
 
-        {
-          "StatusCode": 200,
-          "StatusDescription": "",
-          "Errors": null
-        }
+	{
+	  "StatusCode": 200,
+	  "StatusDescription": "",
+	  "Errors": null
+	}
 
 */
 package titleservice
