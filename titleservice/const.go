3c6a3a2f7c046438c4318f44c35aabb4cd24572d
Peter Hellberg <peter@c7.se>
2017-04-24 11:31:22 +0200

    Update list of MMS channels, rename Eurosport
    (New name of Eurosport is Eurosport 1)

--- titleservice/const.go
+++ titleservice/const.go
@@ -58,7 +58,11 @@ const (
 	TV3                    LiveChannelID = 1028
 	TV4                    LiveChannelID = 1029
 	Kanal9                 LiveChannelID = 1043
+	DiscoveryWorld         LiveChannelID = 1047
+	DiscoveryScience       LiveChannelID = 1048
 	SVTB                   LiveChannelID = 1050
+	TV3SportHD             LiveChannelID = 1052
+	EsportsTV              LiveChannelID = 1055
 	Sjuan                  LiveChannelID = 1061
 	TV4Film                LiveChannelID = 1070
 	TV6                    LiveChannelID = 1074
@@ -67,13 +71,16 @@ const (
 	TV4Guld                LiveChannelID = 1153
 	TV4Komedi              LiveChannelID = 1155
 	TV8                    LiveChannelID = 1197
+	AnimalPlanet           LiveChannelID = 1199
 	SVT24                  LiveChannelID = 1211
+	DiscoveryHDShowcase    LiveChannelID = 1424
 	TV12                   LiveChannelID = 1508
 	Kunskapskanalen        LiveChannelID = 1671
 	TV10                   LiveChannelID = 1683
 	TLC                    LiveChannelID = 1733
 	InvestigationDiscovery LiveChannelID = 1736
-	Eurosport              LiveChannelID = 1906
+	TV4FaktaXL             LiveChannelID = 1796
+	Eurosport1             LiveChannelID = 1906
 	Kanal11                LiveChannelID = 2025
 	Eurosport2Sweden       LiveChannelID = 2047
 )
@@ -88,9 +95,10 @@ func LookupLiveChannelID(name string) (LiveChannelID, bool) {
 func validLiveChannelID(id LiveChannelID) bool {
 	switch id {
 	case
-		SVT1, SVT2, Discovery, Kanal5, TV3, TV4, Kanal9, SVTB, Sjuan, TV4Film, TV6,
-		TV4Sport, TV4Fakta, TV4Guld, TV4Komedi, TV8, SVT24, TV12, Kunskapskanalen,
-		TV10, TLC, InvestigationDiscovery, Eurosport, Kanal11, Eurosport2Sweden:
+		SVT1, SVT2, Discovery, Kanal5, TV3, TV4, Kanal9, DiscoveryWorld, DiscoveryScience,
+		SVTB, TV3SportHD, EsportsTV, Sjuan, TV4Film, TV6, TV4Sport, TV4Fakta, TV4Guld,
+		TV4Komedi, TV8, AnimalPlanet, SVT24, DiscoveryHDShowcase, TV12, Kunskapskanalen,
+		TV10, TLC, InvestigationDiscovery, TV4FaktaXL, Eurosport1, Kanal11, Eurosport2Sweden:
 		return true
 	}
 
@@ -98,29 +106,36 @@ func validLiveChannelID(id LiveChannelID) bool {
 }
 
 var channelLookupTable = map[string]LiveChannelID{
-	"svt1":            SVT1,
-	"svt2":            SVT2,
-	"Discovery":       Discovery,
-	"Kanal5":          Kanal5,
-	"TV3":             TV3,
-	"TV4":             TV4,
-	"Kanal9":          Kanal9,
-	"svtB":            SVTB,
-	"Sjuan":           Sjuan,
-	"TV4 Film":        TV4Film,
-	"TV6":             TV6,
-	"TV4 Sport":       TV4Sport,
-	"TV4 Fakta":       TV4Fakta,
-	"TV4 Guld":        TV4Guld,
-	"TV4 Komedi":      TV4Komedi,
-	"TV8":             TV8,
-	"svt24":           SVT24,
-	"TV12":            TV12,
-	"Kunskapskanalen": Kunskapskanalen,
-	"TV10":            TV10,
-	"TLC":             TLC,
+	"svt1":                  SVT1,
+	"svt2":                  SVT2,
+	"Discovery":             Discovery,
+	"Kanal5":                Kanal5,
+	"TV3":                   TV3,
+	"TV4":                   TV4,
+	"Kanal9":                Kanal9,
+	"Discovery World":       DiscoveryWorld,
+	"Discovery Science":     DiscoveryScience,
+	"svtB":                  SVTB,
+	"TV3 Sport HD":          TV3SportHD,
+	"Esports TV":            EsportsTV,
+	"Sjuan":                 Sjuan,
+	"TV4 Film":              TV4Film,
+	"TV6":                   TV6,
+	"TV4 Sport":             TV4Sport,
+	"TV4 Fakta":             TV4Fakta,
+	"TV4 Guld":              TV4Guld,
+	"TV4 Komedi":            TV4Komedi,
+	"TV8":                   TV8,
+	"Animal Planet":         AnimalPlanet,
+	"svt24":                 SVT24,
+	"Discovery HD Showcase": DiscoveryHDShowcase,
+	"TV12":                  TV12,
+	"Kunskapskanalen":       Kunskapskanalen,
+	"TV10":                  TV10,
+	"TLC":                   TLC,
 	"Investigation Discovery": InvestigationDiscovery,
-	"Eurosport":               Eurosport,
+	"TV4 Fakta XL":            TV4FaktaXL,
+	"Eurosport 1":             Eurosport1,
 	"Kanal 11":                Kanal11,
 	"Eurosport 2 Sweden":      Eurosport2Sweden,
 }
--- titleservice/const_test.go
+++ titleservice/const_test.go
@@ -67,3 +67,50 @@ func TestValidLiveChannelID(t *testing.T) {
 		}
 	}
 }
+
+func TestAllChannels(t *testing.T) {
+	for id, name := range allMMSChannels {
+		if !validLiveChannelID(id) {
+			t.Fatalf("validLiveChannelID(%d) = false, want true", id)
+		}
+
+		if got, ok := LookupLiveChannelID(name); !ok || got != id {
+			t.Fatalf("LookupLiveChannelID(%q) = %q true, want %q true", name, got, id)
+		}
+	}
+}
+
+var allMMSChannels = map[LiveChannelID]string{
+	1001: "svt1",
+	1002: "svt2",
+	1005: "Discovery",
+	1015: "Kanal5",
+	1028: "TV3",
+	1029: "TV4",
+	1043: "Kanal9",
+	1047: "Discovery World",
+	1048: "Discovery Science",
+	1050: "svtB",
+	1052: "TV3 Sport HD",
+	1055: "Esports TV",
+	1061: "Sjuan",
+	1070: "TV4 Film",
+	1074: "TV6",
+	1089: "TV4 Sport",
+	1103: "TV4 Fakta",
+	1153: "TV4 Guld",
+	1155: "TV4 Komedi",
+	1197: "TV8",
+	1199: "Animal Planet",
+	1211: "svt24",
+	1424: "Discovery HD Showcase",
+	1508: "TV12",
+	1671: "Kunskapskanalen",
+	1683: "TV10",
+	1733: "TLC",
+	1736: "Investigation Discovery",
+	1796: "TV4 Fakta XL",
+	1906: "Eurosport 1",
+	2025: "Kanal 11",
+	2047: "Eurosport 2 Sweden",
+}
